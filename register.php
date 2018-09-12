7eb4fe4a12df848d666ff1989ad6a7975ce0b558
Olexandr Borysonok <mebeborn@gmail.com>
2018-09-12 16:58:22 +0300

    2 PHP handlers were created

--- register.php
+++ register.php
@@ -1,48 +1,7 @@
 <?php
-
-function cleanFormString($inputText) {
-	$inputText = strip_tags($inputText);
-	$username = str_replace(" ", "" ,$inputText);
-	return $inputText;
-}
-
-function cleanFormPassword($inputText) {
-	$inputText = strip_tags($inputText);
-	return $inputText;
-}
-
-function cleanFormText($inputText) {
-	$inputText = strip_tags($inputText);
-	$inputText = str_replace(" ", "" ,$inputText);
-	$inputText = ucfirst(strtolower($inputText));
-	return $inputText;
-}
-
-
-if(isset($_POST['loginButton'])) {
-	//Login button was pressed
-
-	$username = cleanFormString($_POST['username']);
-	$password = cleanFormPassword($_POST['password']);
-}
-
-if(isset($_POST['registerButton'])) {
-	//Register button was pressed
-
-	$firstname = cleanFormText($_POST['firstname']);
-	$lastname = cleanFormText($_POST['lastname']);
-
-	$username = cleanFormString($_POST['username']);
-
-	$password = cleanFormPassword($_POST['password']);
-	$password2 = cleanFormPassword($_POST['password2']);
-
-	$email = cleanFormString($_POST['email']);
-}
-
+	include("includes/handlers/login-handler.php");
+	include("includes/handlers/register-handler.php");
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
