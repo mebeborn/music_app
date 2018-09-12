2fe29d4b1f840ec839d599ead943b07f5e1f256c
Olexandr Borysonok <mebeborn@gmail.com>
2018-09-12 15:26:19 +0300

    Initial commit of the project with html login form

---
+++ register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcomy to Music App</title>
+</head>
+<body>
+
+	<div id="inputContainer">
+		<form action="register.php" id="loginForm">
+			<h2>Login your account</h2>
+			<p>
+				<label for="loginUsername">Username</label>
+				<input type="text" id="loginUsername" name="loginUsername" placeholder="alexborn" required>
+			</p>
+			<p>
+				<label for="loginPassword">Password</label>
+				<input type="text" id="loginPassword" name="loginPassword" required>
+			</p>
+
+			<button type="submit" name="loginButton">Log In</button>
+		</form>
+	</div>
+	
+</body>
+</html>
