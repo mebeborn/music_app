8cbf7d0de0455cf7b7c37238f36c20f95cb6e970
Olexandr Borysonok <mebeborn@gmail.com>
2018-09-12 16:40:30 +0300

    Get input data and prepare it with PHP

--- register.php
+++ register.php
@@ -1,7 +1,45 @@
 <?php
-if(isset($_POST['loginButton'])) {
-	echo "LOGIN button IS PRESSED";
+
+function cleanFormString($inputText) {
+	$inputText = strip_tags($inputText);
+	$username = str_replace(" ", "" ,$inputText);
+	return $inputText;
 }
+
+function cleanFormPassword($inputText) {
+	$inputText = strip_tags($inputText);
+	return $inputText;
+}
+
+function cleanFormText($inputText) {
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "" ,$inputText);
+	$inputText = ucfirst(strtolower($inputText));
+	return $inputText;
+}
+
+
+if(isset($_POST['loginButton'])) {
+	//Login button was pressed
+
+	$username = cleanFormString($_POST['username']);
+	$password = cleanFormPassword($_POST['password']);
+}
+
+if(isset($_POST['registerButton'])) {
+	//Register button was pressed
+
+	$firstname = cleanFormText($_POST['firstname']);
+	$lastname = cleanFormText($_POST['lastname']);
+
+	$username = cleanFormString($_POST['username']);
+
+	$password = cleanFormPassword($_POST['password']);
+	$password2 = cleanFormPassword($_POST['password2']);
+
+	$email = cleanFormString($_POST['email']);
+}
+
 ?>
 
 
