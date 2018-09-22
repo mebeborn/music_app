613d65aacd6a2fc0029281a57b68f33020a336c8
Olexandr Borysonok <mebeborn@gmail.com>
2018-09-22 12:25:11 +0300

    Navbar Started

--- index.php
+++ index.php
@@ -17,100 +17,111 @@
 	<link rel="stylesheet" href="assets/css/indexx.css">
 </head>
 <body>
-	<!-- <?php echo"<h1>Hello $userLoggedIn</h1>" ?> -->
 
-	<div id="nowPlayingBarContainer">
+	
+	<div class="mainContainer">
+		
+		<div id="topContainer">
+			
+			<div id="navBarContainer">
+						
+			</div>
+		
+		</div>
+	
+		<div id="nowPlayingBarContainer">
 
-		<div id="nowPlayingBar">
+			<div id="nowPlayingBar">
 
-			<div id="nowPlayingLeft">
+				<div id="nowPlayingLeft">
 
-				<div class="content">
+					<div class="content">
 
-					<span class="albumLink">
-						<img src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumArtWork">
-					</span>
-
-					<div class="trackInfo">
-						<span class="trackName">
-							<span>Happy Birthday</span>
+						<span class="albumLink">
+							<img src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumArtWork">
 						</span>
 
-						<span class="artistName">
-							<span>Nikol Likon</span>
-						</span>
+						<div class="trackInfo">
+							<span class="trackName">
+								<span>Happy Birthday</span>
+							</span>
+
+							<span class="artistName">
+								<span>Nikol Likon</span>
+							</span>
+						</div>
+
+
 					</div>
+				</div>
+				
+				<div id="nowPlayingCenter">
 
+					<div class="content playerControls">
+
+						<div class="buttons">
+							
+							<button class="controlButton shuffle" title="Shuffle button">
+								<img src="assets/images/icons/shuffle.png" alt="Shuffle">
+							</button>
+
+							<button class="controlButton previous" title="Previous button">
+								<img src="assets/images/icons/previous.png" alt="previous">
+							</button>
+
+							<button class="controlButton play" title="Play button">
+								<img src="assets/images/icons/play.png" alt="Play">
+							</button>
+
+							<button class="controlButton pause" title="Pause button" style="display:none;">
+								<img src="assets/images/icons/pause.png" alt="Pause">
+							</button>
+
+							<button class="controlButton next" title="Next button">
+								<img src="assets/images/icons/next.png" alt="Next">
+							</button>
+
+							<button class="controlButton repeat" title="Repeat button">
+								<img src="assets/images/icons/repeat.png" alt="Repeat">
+							</button>
+
+						</div>
+
+						<div class="playbackBar">
+							<span class="progressTime current">0.00</span>
+
+							<div class="progressBar">
+								<div class="progressBarBg">
+									<div class="progress"></div>
+								</div>
+							</div>
+							
+							<span class="progressTime remaining">0.00</span>
+						</div>
+
+					</div>
 
 				</div>
-			</div>
-			
-			<div id="nowPlayingCenter">
+				
+				<div id="nowPlayingRight">
+					<div class="volumeBar">
 
-				<div class="content playerControls">
-
-					<div class="buttons">
-						
-						<button class="controlButton shuffle" title="Shuffle button">
-							<img src="assets/images/icons/shuffle.png" alt="Shuffle">
+						<button class="controlButton volume" title="Volume button">
+							<img src="assets/images/icons/volume.png" alt="Volume">
 						</button>
-
-						<button class="controlButton previous" title="Previous button">
-							<img src="assets/images/icons/previous.png" alt="previous">
-						</button>
-
-						<button class="controlButton play" title="Play button">
-							<img src="assets/images/icons/play.png" alt="Play">
-						</button>
-
-						<button class="controlButton pause" title="Pause button" style="display:none;">
-							<img src="assets/images/icons/pause.png" alt="Pause">
-						</button>
-
-						<button class="controlButton next" title="Next button">
-							<img src="assets/images/icons/next.png" alt="Next">
-						</button>
-
-						<button class="controlButton repeat" title="Repeat button">
-							<img src="assets/images/icons/repeat.png" alt="Repeat">
-						</button>
-
-					</div>
-
-					<div class="playbackBar">
-						<span class="progressTime current">0.00</span>
 
 						<div class="progressBar">
 							<div class="progressBarBg">
 								<div class="progress"></div>
 							</div>
 						</div>
-						
-						<span class="progressTime remaining">0.00</span>
+
 					</div>
-
-				</div>
-
-			</div>
-			
-			<div id="nowPlayingRight">
-
-				<div class="volumeBar">
-
-					<button class="controlButton volume" title="Volume button">
-						<img src="assets/images/icons/volume.png" alt="Volume">
-					</button>
-
-					<div class="progressBar">
-						<div class="progressBarBg">
-							<div class="progress"></div>
-						</div>
-					</div>
-
 				</div>
 
 			</div>
 		</div>
+
 	</div>
 </body>
 </html>
