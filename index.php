e9c59ed3f3df635834ae19addf7c15ccdcb8cece
Olexandr Borysonok <mebeborn@gmail.com>
2018-09-24 11:57:07 +0300

    Connect to DB and show Album titles

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php include("header.php"); ?>
 						
+<h1 class="pageHeadingBig">You Might Also like</h1>
+
+<div class="gridViewContainer">
+	
+	<?php
+		$albumQuery = mysqli_query($con, "SELECT * FROM albums");
+
+		while($row = mysqli_fetch_array($albumQuery)) {
+			echo $row['title'];
+		}
+	?>
+
+</div>
+
 <?php include("footer.php"); ?>
-			
