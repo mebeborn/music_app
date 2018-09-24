ffc20eb34fec2fdbd2bde62d0338add63bbe7d73
Olexandr Borysonok <mebeborn@gmail.com>
2018-09-24 12:30:44 +0300

    Albums are clickable

--- index.php
+++ index.php
@@ -10,12 +10,13 @@
 		while($row = mysqli_fetch_array($albumQuery)) {
 
 			echo "<div class='gridViewItem'>
-					
-					<img src='" . $row['artworkPath'] . "'>
-					
-					<div class='gridViewInfo'>"
-						. $row['title'] .		
-					"</div>
+					<a href='album.php?id=" . $row['id'] . "'>
+						<img src='" . $row['artworkPath'] . "'>
+						
+						<div class='gridViewInfo'>"
+							. $row['title'] .		
+						"</div>
+					</a>
 				</div>";
 
 
