a612b112cf8a751a11b263241eaf9299ad0a8b99
Olexandr Borysonok <mebeborn@gmail.com>
2018-09-24 14:26:30 +0300

    Output all tracks

--- album.php
+++ album.php
@@ -36,10 +36,25 @@ $artist = $album->getArtist();
 
 
 			echo "<li class='tracklistRow'>
+
 					<div class='trackCount'>
-					<img class='play' src='assets/images/icons/play-white.png'>
+						<img class='play' src='assets/images/icons/play-white.png'>
 						<span class='trackNumber'>$i</span>
 					</div>
+
+					<div class='trackInfo'>
+						<span class='trackName'>".$albumSong->getTitle()."</span>
+						<span class='artistName'>".$albumArtist->getName()."</span>
+					</div>
+					
+					<div class='trackOptions'>
+						<img class='optionButton' src='assets/images/icons/more.png'>
+					</div>
+
+					<div class='trackDuration'>
+						<span class='duration'>".$albumSong->getDuration()."</span>
+					</div>
+
 				</li>";
 
 			$i++;
